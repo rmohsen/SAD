0db5011bbe12386a87662b42b508365a258a838e
Armin <arminbehnamnia@gmail.com>
2017-10-31 19:41:35 +0330

    Structure of project created. A test page can be invoked by localhost:8000/test/. About the files: bl.py: all backend logic should go here. dao.py: access to database should be handled here. dto: data transfer object is the container used in case part of an entity is needed to be transfered from bl to controller(view.py).

--- S1/views.py
+++ S1/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from jinja2 import Template
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse("<html><body>Hello guys!</body></html>")
