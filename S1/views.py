2d768c86d294a764192c2447bb4a89573ad521b1
Armin <arminbehnamnia@gmail.com>
2017-11-01 17:52:46 +0330

    listStudents usecase inserted into the container page.

--- S1/views.py
+++ S1/views.py
@@ -6,11 +6,13 @@ from django.urls import reverse
 # Create your views here.
 
 def index(request):
-    env = Environment(loader=PackageLoader('S1', 'Web/templates'), autoescape=select_autoescape(['html', 'xml']))
+    env = Environment(loader=PackageLoader('S1', 'Web/templates'))
     env.globals.update({'static': staticfiles_storage.url("S1/"), 'url': reverse})
-    template = env.get_template('container.html')
-    return HttpResponse(template.render(data={'header': ['نام', 'نام خانوادگی', 'تاریخ تولد', 'کد ملی'],
+    template = env.get_template('list.html')
+    list_content = template.render(data={'header': ['نام', 'نام خانوادگی', 'تاریخ تولد', 'کد ملی'],
                                          'body': [['آرمین', 'بهنام نیا', '16/8/1374', '0018569773'],
                                                   ['آرش', 'پوردامغانی', '16/8/1374', '0010010017'],
                                                   ['محسن', 'رحیمی', '16/8/1374', '0018569773']]
-                                         }))
+                                         })
+    template = env.get_template('container.html')
+    return HttpResponse(template.render(list_content=list_content))
