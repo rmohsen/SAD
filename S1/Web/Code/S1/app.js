50ec0395c11eb3b8898a3d97615ba3b0d18d174f
Armin <arminbehnamnia@gmail.com>
2017-11-03 18:33:00 +0330

    Urls corrected. Front-end progressed.

--- S1/Web/Code/S1/app.js
+++ S1/Web/Code/S1/app.js
@@ -5,6 +5,9 @@ $(document).ready(function () {
     $("input[type='radio'][name='tab-select']").change(function () {
         console.log(this.value);
         change_tab(this.value);
+        if (this.value === "1"){
+            getList();
+        }
     });
 });
 
@@ -18,4 +21,48 @@ function clear_all_tabs(){
     for (var i = 0; i < x.length; i++){
         $(x[i]).css("display", "none");
     }
+}
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+var csrftoken;
+$(document).ready(function () {
+    csrftoken = $("input[name='csrfmiddlewaretoken']").value;
+    alert(csrftoken);
+});
+function getList(){
+    $.ajax({
+        url: "getList",
+        type: "POST",
+        data: {
+                "X-CSRFToken": csrftoken
+            },
+        success: function(result){
+            $(".tab-container .tab-contents #tab-content-1").html(result);
+        },
+        error: function (response) {
+            $(".tab-container .tab-contents #tab-content-1").html(response.responseText);
+        }
+    });
+}
+function addStudent(){
+    $.ajax({
+        url: "addStudent",
+        type: "POST",
+        data:{
+
+        }
+    });
 }
